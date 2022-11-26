a4fc0ce33a935ea04a83e1b9ca957362d0cc9b3f
Ziklingr <t1kapa01@students.oamk.fi>
2022-11-26 19:20:19 +0200

    lisätty takaisin napit valikoihin

--- frontend/mainwindow.h
+++ frontend/mainwindow.h
@@ -29,6 +29,16 @@ private slots:
 
     void on_LukitseKortti_clicked();
 
+    void on_Takaisin_clicked();
+
+    void on_takaisin_clicked();
+
+    void on_TakaisinN_clicked();
+
+    void on_TakaisinT_clicked();
+
+    void on_TakaisinS_clicked();
+
 private:
     Ui::MainWindow *ui;
     QNetworkAccessManager *loginManager;
